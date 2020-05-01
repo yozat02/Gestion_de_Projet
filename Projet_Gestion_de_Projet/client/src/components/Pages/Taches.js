645ce8cf085619079ee4136cda411c4d84d85d6e
yziat <yziat@sqli.com>
2020-05-01 01:42:02 +0000

    Add Developpeur front

--- Projet_Gestion_de_Projet/client/src/components/Pages/Taches.js
+++ Projet_Gestion_de_Projet/client/src/components/Pages/Taches.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import {TreeTable} from "../Home/TreeTable/TreeTable";
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import { useMutation } from '@apollo/react-hooks';
 
 const DEVELOPPEURS = gql`
 query developpeurByTacheId($tacheId: ID!) {
@@ -11,6 +12,13 @@ query developpeurByTacheId($tacheId: ID!) {
     name 
   }
 }
+`;
+const ADD_DEVELOPPEUR = gql`
+  mutation createDeveloppeur($input: DeveloppeurInput) {
+    createDeveloppeur(input: $input) {
+      name
+    }
+  }
 `;
 
 const useStyles = makeStyles((theme) => ({
@@ -24,6 +32,15 @@ const CheckupsTachesPage = ({match}) => {
   console.log("match",match)
  
   let { loading, error, data } = useQuery(DEVELOPPEURS,{ variables: {tacheId: match.params.id},});
+  const [addDev] = useMutation(ADD_DEVELOPPEUR);
+  const addItem = (name) => {
+    addDev({
+      variables: {
+          input: {"tacheId":match.params.id,"name": name },
+          refetchQueries: [{ query: DEVELOPPEURS }],
+      }
+     });
+    }
 
   if (loading) return <p>Loading...</p>;
   let array = []
@@ -35,7 +52,7 @@ const CheckupsTachesPage = ({match}) => {
   return (
     
       <div className={classes.allWidth}>
-        <TreeTable title={"Liste des developpeurs"} tableData={array} />
+        <TreeTable title={"Liste des developpeurs"} tableData={array} addItem={addItem} />
       </div>
     
   );
