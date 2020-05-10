360ab8f1eddc4a5c96a1ebe341727aa1934d0481
yziat <yziat@sqli.com>
2020-05-10 01:00:18 +0000

    Fix Date type

--- Projet_Gestion_de_Projet/client/src/components/Pages/ListTache.js
+++ Projet_Gestion_de_Projet/client/src/components/Pages/ListTache.js
@@ -19,13 +19,25 @@ export const ListTache = ({taches,type}) => {
     return (
         <div>
             <List >
-               {!!list && list.map(tache => (
-                <ListItem>
+               {!!list && list.map(tache => {
+                let text = null ;
+                if(type=="1" || type =="3"){
+                    text = "Date fin :"+ (new Date(tache.dateFin).getDate()<10 ? "0"+new Date(tache.dateFin).getDate():new Date(tache.dateFin).getDate())
+                    +"/"+(new Date(tache.dateFin).getMonth()<10 ? "0"+(new Date(tache.dateFin).getMonth()+1):(new Date(tache.dateFin).getMonth()+1))
+                    +"/"+new Date(tache.dateFin).getFullYear()
+                }
+                else{
+                    text = "Date debut :"+ (new Date(tache.dateDebut).getDate()<10 ? "0"+new Date(tache.dateDebut).getDate():new Date(tache.dateDebut).getDate())
+                    +"/"+(new Date(tache.dateDebut).getMonth()<10 ? "0"+(new Date(tache.dateDebut).getMonth()+1) :(new Date(tache.dateDebut).getMonth()+1))
+                    +"/"+new Date(tache.dateDebut).getFullYear()
+                }
+                return <ListItem>
                 <ListItemText
-                 primary={tache.name+" : "+tache.description }
+                 primary={tache.name+" : "+text }
                 />
             </ListItem>
-               ) )}     
+            }
+                )}     
             </List>
        </div>
       );
